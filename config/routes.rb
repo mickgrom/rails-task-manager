78d1115410cd2d4c773410484eb5c68526d35282
Mick Grom <mickgrom@gmail.com>
2018-06-05 17:10:55 +0200

    question 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # INDEX
+  # root to: 'tasks#index'
+  # resources :tasks
+
+
+  get "tasks",           to: "tasks#index", as: :index
+
+  # create
+  get "tasks/new",       to: "tasks#new", as: :new
+
+  # SHOW
+  get "tasks/:id",       to: "tasks#show", as: :task
+
+  post "tasks",          to: "tasks#create"
+
+  delete "tasks/:id",    to: "tasks#destroy", as: :destroy
+
+  # EDIT / UPDATE
+  get "tasks/:id/edit",  to: "tasks#edit", as: :edit
+  patch "tasks/:id",     to: "tasks#update"
+
+
+
+
 end
+
+
+
